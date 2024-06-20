12e5497325fc40c5673ab9a4c8f80e29febe21c3
jamamanica <jamamanica>
2024-06-19 22:06:33 -0500

    Actualiza ejercicio 1 de estructuras repetitivas: optimiza el codigo

--- sumaprimerosn.cpp
+++ sumaprimerosn.cpp
@@ -2,14 +2,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n, s, i;
+    int n, s=0;
     cout<<"Ingrese la cantidad de numeros a sumar: ";
     cin>>n;
-    s=0;
-    i=1;
-    while(i<=n){
+    for(int i=1;i<=n;i++){
         s=s+i;
-        i=i+1;
     }
     cout<<"La sumatoria de los "<<n<<" primeros numeros es: "<<s<<endl;
     return 0;

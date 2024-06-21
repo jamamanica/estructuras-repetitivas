d9cff66b3b9f7da2f02de87488b95ee67f89373b
jamamanica <jamamanica>
2024-06-20 20:59:08 -0500

    Actualiza ejercicio 1: nueva funcionalidad y mejora interfaz

--- sumaprimerosn.cpp
+++ sumaprimerosn.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 int main(){
     int n, s=0;
+    cout<<"<<SUMA DE NUMEROS PARES>>"<<endl;
     cout<<"Ingrese la cantidad de numeros a sumar: ";
     cin>>n;
-    for(int i=1;i<=n;i++){
+    for(int i=2;i<=2*n;i=i+2){
         s=s+i;
     }
-    cout<<"La sumatoria de los "<<n<<" primeros numeros es: "<<s<<endl;
+    cout<<"La sumatoria de los "<<n<<" primeros numeros pares es: "<<s<<endl;
     return 0;
 }
